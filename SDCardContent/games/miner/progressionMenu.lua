6b06b08e20f326b2b15fa6d25a8414337e27d145
Dakkaron <dak1st@gmx.at>
2025-06-25 21:52:06 +0200

    Added miner game stage 7 data;refactoring progression menu

--- SDCardContent/games/miner/progressionMenu.lua
+++ SDCardContent/games/miner/progressionMenu.lua
@@ -6,58 +6,17 @@ DrawString("Geld: $" .. Money, 170, 10)
 SetTextSize(2)
 
 if (Stage == 1) then
-  if (Money < 2000) then
-    FillRect(10, 60, 220, 35, 0xF800)
-  else
-    FillRect(10, 60, 220, 35, 0x001F)
-    SetTextSize(1)
-    DrawString("Nicht genug Geld", 15, 84)
-  end
-
-  SetTextSize(2)
-  DrawString("Tiefer graben: $2000", 15, 65)
-  if (Money >= 2000 and IsTouchInZone(10, 60, 220, 35)) then
-    Money = Money - 2000
-    Stage = 2
-    PrefsSetInt("stage", Stage)
-    PrefsSetInt("money", Money)
-  end
+  DisplayBuyStage(2000)
 elseif (Stage == 2) then
-  if (Money < 10000) then
-    FillRect(10, 60, 220, 35, 0xF800)
-  else
-    FillRect(10, 60, 220, 35, 0x001F)
-    SetTextSize(1)
-    DrawString("Nicht genug Geld", 15, 84)
-  end
-
-
-  SetTextSize(2)
-  DrawString("Tiefer graben: $10000", 15, 65)
-  if (Money >= 10000 and IsTouchInZone(10, 60, 220, 35)) then
-    Money = Money - 10000
-    Stage = 3
-    PrefsSetInt("stage", Stage)
-    PrefsSetInt("money", Money)
-  end
+  DisplayBuyStage(5000)
 elseif (Stage == 3) then
-  if (Money < 25000) then
-    FillRect(10, 60, 220, 35, 0xF800)
-  else
-    FillRect(10, 60, 220, 35, 0x001F)
-    SetTextSize(1)
-    DrawString("Nicht genug Geld", 15, 84)
-  end
-
-
-  SetTextSize(2)
-  DrawString("Tiefer graben: $25000", 15, 65)
-  if (Money >= 25000 and IsTouchInZone(10, 60, 220, 35)) then
-    Money = Money - 25000
-    Stage = 4
-    PrefsSetInt("stage", Stage)
-    PrefsSetInt("money", Money)
-  end
+  DisplayBuyStage(15000)
+elseif (Stage == 4) then
+  DisplayBuyStage(25000)
+elseif (Stage == 5) then
+  DisplayBuyStage(35000)
+elseif (Stage == 6) then
+  DisplayBuyStage(40000)
 end
 
 SetTextSize(2)
