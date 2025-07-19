15000588b8337343416276aab0cd6498b7a39d9b
Dakkaron <dak1st@gmx.at>
2025-07-20 01:27:08 +0200

    Sky, birds and drowning for plane game

--- SDCardContent/games/plane/init.lua
+++ SDCardContent/games/plane/init.lua
@@ -1,4 +1,5 @@
 SBackground = LoadSprite("gfx/background.bmp")
+SSky = LoadSprite("gfx/sky.bmp")
 SPlane = LoadSprite("gfx/plane.bmp", 0, 0xf81f)
 SPlaneBankL = LoadSprite("gfx/planeBankL.bmp", 0, 0xf81f)
 SPlaneBankR = LoadSprite("gfx/planeBankR.bmp", 0, 0xf81f)
@@ -10,27 +11,38 @@ SSplash = {
   LoadSprite("gfx/splash2.bmp", 0, 0xf81f),
 }
 SStorm = LoadSprite("gfx/storm.bmp", 0, 0xf81f)
+SBird = LoadSprite("gfx/bird.bmp", 0, 0xf81f)
 CameraAngle = 0
 CameraX = 0
 CameraY = 0
 CameraHeight = 1
 Speed = 100
+TopFlightHeight = 2
 
+DrowningStartMs = 0
 TargetShip = 1
 Money = PrefsGetInt("money", 0)
 
-HorizonHeight = 50
+HorizonHeight = 49
 
-ObjectTypeShip = 1
-ObjectTypeStorm = 2
+BIRD_SPEED = 20
+
+OBJECT_TYPE_SHIP = 1
+OBJECT_TYPE_STORM = 2
+OBJECT_TYPE_BIRD = 3
 
 Objects = {
-  {ObjectTypeShip, 0, 100, 0, 1},
-  {ObjectTypeShip, 400, 400, 0, 2},
-  {ObjectTypeShip, 0, 380, 0, 3},
-  {ObjectTypeShip, 400, 100, 0, 4},
-  {ObjectTypeShip, 200, 200, 0, 5}
-  --{ObjectTypeStorm, 0, 200, 0, 1},
+  {type = OBJECT_TYPE_SHIP, x = 0,   y = 100, d = 0, id = 1},
+  {type = OBJECT_TYPE_SHIP, x = 400, y = 400, d = 0, id = 2},
+  {type = OBJECT_TYPE_SHIP, x = 0,   y = 380, d = 0, id = 3},
+  {type = OBJECT_TYPE_SHIP, x = 400, y = 100, d = 0, id = 4},
+  {type = OBJECT_TYPE_SHIP, x = 200, y = 200, d = 0, id = 5},
+  {type = OBJECT_TYPE_STORM, x = 200, y = 300, d = 0},
+  {type = OBJECT_TYPE_BIRD, x = 0, y = 80, d = 0, targetX = math.random(10, 450), targetY = math.random(10, 450)},
+  {type = OBJECT_TYPE_BIRD, x = math.random(10, 450), y = math.random(10, 450), d = 0, targetX = math.random(10, 450), targetY = math.random(10, 450)},
+  {type = OBJECT_TYPE_BIRD, x = math.random(10, 450), y = math.random(10, 450), d = 0, targetX = math.random(10, 450), targetY = math.random(10, 450)},
+  {type = OBJECT_TYPE_BIRD, x = math.random(10, 450), y = math.random(10, 450), d = 0, targetX = math.random(10, 450), targetY = math.random(10, 450)},
+  {type = OBJECT_TYPE_BIRD, x = math.random(10, 450), y = math.random(10, 450), d = 0, targetX = math.random(10, 450), targetY = math.random(10, 450)},
   --{ObjectTypeStorm, 50, 300, 0, 2}
 }
 
@@ -41,21 +53,23 @@ function calcDistance2D(x1, y1, x2, y2)
   return math.sqrt(dx*dx + dy*dy)
 end
 
-function drawBillboard(sprite, worldX, worldY, worldHeight, baseScale)
+function drawBillboard(sprite, worldX, worldY, worldHeight, baseScale, hflip)
   local x,y,z = Mode7WorldToScreen(worldX,worldY, CameraX,CameraY,CameraHeight,CameraAngle, 1, HorizonHeight, 20,180)
   local dx = worldX - CameraX
   local dy = worldY - CameraY
-  local dz = (worldHeight - CameraHeight) * 5
+  local dz = -worldHeight * 35--(worldHeight - CameraHeight) * 5
   local distance = math.sqrt(dx*dx + dy*dy)
   scale = 15/distance
-  local sy = y + dz * scale
-  DrawSpriteScaled(sprite, x, sy, scale*baseScale, scale*baseScale, 0x09)
-  DrawString("x ".. x, 30, 70)
-  DrawString("y ".. sy, 30, 80)
-  DrawString("s ".. scale, 30, 90)
+  local sy = y + dz * scale + dz * scale
+  if (distance < 50) then
+    DrawString("y  " .. y, 250, 20)
+    DrawString("dz " .. dz, 250, 30)
+    DrawString("sy " .. sy, 250, 40)
+  end
+  DrawSpriteScaled(sprite, x, sy, hflip and -scale*baseScale or scale*baseScale, scale*baseScale, 0x05)
 end
 
-function drawShip(sprite, worldX, worldY, isTarget)
+function drawShip(sprite, worldX, worldY)
   local x,y,z = Mode7WorldToScreen(worldX,worldY, CameraX,CameraY,CameraHeight,CameraAngle, 1, HorizonHeight, 20,180)
   if (x==-1000 and y==-1000) then
     return
@@ -72,14 +86,31 @@ function drawShip(sprite, worldX, worldY, isTarget)
       FillRect(x-SpriteWidth(sprite)*scale, 181, SpriteWidth(sprite)*scale*2, y+SpriteWidth(sprite)*scale-181, 0x0000)
     end
   end
-  if (isTarget) then
-    if (distance<8) then
-      DrawSpriteScaled(SRing, x, 120, scale, scale, 0x05)
-    elseif (distance<50) then
-      DrawSpriteScaled(SRing, x, y-100*scale, scale, scale, 0x09)
-    else
-      DrawSprite(SDownArrow, x-SpriteWidth(SDownArrow)/2, 25 + math.sin(Ms/100))
+end
+
+function drawTarget(sprite, worldX, worldY)
+  --local x,y,z = Mode7WorldToScreen(worldX,worldY, CameraX,CameraY,CameraHeight,CameraAngle, 1, HorizonHeight, 20,180)
+  --if (x==-1000 and y==-1000) then
+  --  return
+  --end
+  local dx = CameraX - worldX
+  local dy = CameraY - worldY
+  local distance = math.sqrt(dx*dx + dy*dy)
+  --if (distance<8 and (TopFlightHeight - CameraHeight) < (TopFlightHeight/10.0)) then
+  --  DrawSpriteScaled(SRing, x, 120, scale, scale, 0x05)
+  --elseif (distance<50) then
+    --DrawSpriteScaled(SRing, x, y-100*scale, scale, scale, 0x09)
+  --else
+    --DrawSprite(SDownArrow, x-SpriteWidth(SDownArrow)/2, 25 + math.sin(Ms/100))
+  --end
+  if (distance<50) then
+    drawBillboard(SRing, worldX, worldY, 2, 1, false)
+  else
+    local x,y,z = Mode7WorldToScreen(worldX,worldY, CameraX,CameraY,CameraHeight,CameraAngle, 1, HorizonHeight, 20,180)
+    if (x==-1000 and y==-1000) then
+      return
     end
+    DrawSprite(SDownArrow, x-SpriteWidth(SDownArrow)/2, 25 + math.sin(Ms/100))
   end
 end
 
@@ -102,5 +133,46 @@ function DisplayEarnings(x, y)
     end
   else
     EarnValue = 0
+  end
+end
+
+function getObjectRelativeDirection(x, y)
+  local targetAngle = math.atan(y, x)
+  local angleDiff = CameraAngle - targetAngle
+  if (angleDiff<-math.pi) then
+    angleDiff = angleDiff + 2*math.pi
+  end
+  return angleDiff
+end
+
+function getObjectMotionVector(object)
+  dx = object.targetX - object.x
+  dy = object.targetY - object.y
+  m = math.sqrt(dx*dx + dy*dy)
+  return dx/m, dy/m
+end
+
+-- Progression Menu
+PART_TYPE_PLANE = 1
+PART_TYPE_PROPELLER = 2
+PART_TYPE_ENGINE = 3
+PART_TYPE_WINGS = 4
+
+function DisplayBuyPart(price, partType)
+  if (Money < price) then
+    FillRect(10, 60, 220, 35, 0xF800)
+  else
+    FillRect(10, 60, 220, 35, 0x001F)
+    SetTextSize(1)
+    DrawString("Nicht genug Geld", 15, 84)
+  end
+
+  SetTextSize(2)
+  DrawImage()
+  if (Money >= price and IsTouchInZone(10, 60, 220, 35)) then
+    Money = Money - price
+    Stage = Stage + 1
+    PrefsSetInt("stage", Stage)
+    PrefsSetInt("money", Money)
   end
 end
--- SDCardContent/games/plane/longBlow.lua
+++ SDCardContent/games/plane/longBlow.lua
@@ -1,39 +1,81 @@
+DisableCaching()
 CameraHeight = CameraHeight + (Speed-50) * MsDelta * 0.00001
-CameraHeight = Constrain(CameraHeight, 0.1, 2)
-if NewRepetition then
-  Speed = Speed + 100
+if (DrowningStartMs == 0 and CameraHeight < 0.1 ) then
+  DrowningStartMs = Ms
 end
-speedDrop = 1 - (0.0001 * MsDelta)
-Speed = Speed * speedDrop
-CameraX = CameraX - math.sin(CameraAngle) * Speed * 0.0001 * MsDelta
-CameraY = CameraY + math.cos(CameraAngle) * Speed * 0.0001 * MsDelta
+CameraHeight = Constrain(CameraHeight, 0.1, TopFlightHeight)
+if (DrowningStartMs == 0) then
+  if NewRepetition then
+    Speed = Speed + 100
+  end
+  speedDrop = 1 - (0.0001 * MsDelta)
+  Speed = Speed * speedDrop
+  CameraX = CameraX - math.sin(CameraAngle) * Speed * 0.0001 * MsDelta
+  CameraY = CameraY + math.cos(CameraAngle) * Speed * 0.0001 * MsDelta
+end
 
-DrawMode7(SBackground, CameraX, CameraY, CameraHeight, CameraAngle, 1, HorizonHeight, 20, 180)
+local realHorizonHeight = HorizonHeight - 10 + CameraHeight*5
+local skyXOffset = math.fmod(CameraAngle*160, SpriteWidth(SSky))
+DrawSprite(SSky, skyXOffset, 0)
+if (SpriteWidth(SSky) + skyXOffset < 320) then
+  DrawSprite(SSky, skyXOffset + SpriteWidth(SSky), 0)
+end
+if (skyXOffset > 0) then
+  DrawSprite(SSky, skyXOffset - SpriteWidth(SSky), 0)
+end
+DrawMode7(SBackground, CameraX, CameraY, CameraHeight, CameraAngle, 1, realHorizonHeight, 20, 180)
 
 for i = 1, #Objects do
-  Objects[i][4] = calcDistance2D(CameraX, CameraY, Objects[i][2], Objects[i][3])
+  Objects[i].d = calcDistance2D(CameraX, CameraY, Objects[i].x, Objects[i].y)
 end
 
 table.sort(Objects, function (left, right)
-  return left[4] > right[4]
+  return left.d > right.d
 end)
 
 local targetShipData = {}
 local totalShipCount = 0
 for i = 1, #Objects do
-  local objectType = Objects[i][1]
-  if (objectType == ObjectTypeShip) then
-    drawShip(SSailShip, Objects[i][2], Objects[i][3], Objects[i][5] == TargetShip)
-    if Objects[i][5] == TargetShip then
-      targetShipData = Objects[i]
-	end
-	totalShipCount = totalShipCount + 1
-  elseif (objectType == ObjectTypeStorm) then
-    drawBillboard(SStorm, Objects[i][2], Objects[i][3], 1, 10)
+  local object = Objects[i]
+  local objectType = object.type
+  if (objectType == OBJECT_TYPE_SHIP) then
+    drawShip(SSailShip, object.x, object.y)
+    if object.id == TargetShip then
+      targetShipData = object
+    end
+    totalShipCount = totalShipCount + 1
+  elseif (objectType == OBJECT_TYPE_STORM and object.d < 200) then
+    drawBillboard(SStorm, object.x, object.y, 7, 10)
+  elseif (objectType == OBJECT_TYPE_BIRD and object.d < 1000) then
+    mx,my = getObjectMotionVector(object)
+    object.x = object.x + mx * BIRD_SPEED * 0.001 * MsDelta
+    object.y = object.y + mx * BIRD_SPEED * 0.001 * MsDelta
+    local dir = getObjectRelativeDirection(mx, my)
+    drawBillboard(SBird, object.x, object.y, 10, 0.5, dir > 0 and true or false)
+    local dx = object.targetX-object.x
+    local dy = object.targetY-object.y
+    local targetDistance = math.sqrt(dx*dx + dy*dy)
+    if (targetDistance < 10) then
+      object.targetX = math.random(10, 450)
+      object.targetY = math.random(10, 450)
+    end
   end
 end
 
-if (calcDistance2D(CameraX, CameraY, targetShipData[2], targetShipData[3]) < 3) then
+drawTarget(SSailShip, targetShipData.x, targetShipData.y)
+
+for i = 1, #Objects do
+  local object = Objects[i]
+  if (object.type == OBJECT_TYPE_STORM and object.d < 40) then
+    CameraHeight = CameraHeight - MsDelta * 0.0001 * 10 - (Speed-50) * MsDelta * 0.00001
+    if (DrowningStartMs == 0 and CameraHeight < 0.1 ) then
+      DrowningStartMs = Ms
+    end
+    CameraHeight = Constrain(CameraHeight, 0.1, TopFlightHeight)
+  end
+end
+
+if (calcDistance2D(CameraX, CameraY, targetShipData.x, targetShipData.y) < 3 and (TopFlightHeight - CameraHeight) < (TopFlightHeight/10.0)) then
   TargetShip = TargetShip + 1
   AddEarnings(5)
   if (TargetShip > totalShipCount) then
@@ -42,18 +84,47 @@ if (calcDistance2D(CameraX, CameraY, targetShipData[2], targetShipData[3]) < 3) 
 end
 
 local planeScale = 0.85+CameraHeight/8
-if (IsTouchInZone(0, 0, 100, 240)) then
-  DrawSpriteScaled(SPlaneBankL, 160, 120, planeScale, planeScale, 0x05)
-  CameraAngle = CameraAngle + 0.001 * MsDelta
-elseif (IsTouchInZone(220, 0, 100, 240)) then
-  DrawSpriteScaled(SPlaneBankR, 160, 120, planeScale, planeScale, 0x05)
-  CameraAngle = CameraAngle - 0.001 * MsDelta
+if (DrowningStartMs == 0) then
+  if (IsTouchInZone(0, 0, 100, 240)) then
+    DrawSpriteScaled(SPlaneBankL, 160, 120, planeScale, planeScale, 0x05)
+    CameraAngle = CameraAngle + 0.001 * MsDelta
+    CameraAngle = math.fmod(CameraAngle, math.pi*2)
+  elseif (IsTouchInZone(220, 0, 100, 240)) then
+    DrawSpriteScaled(SPlaneBankR, 160, 120, planeScale, planeScale, 0x05)
+    CameraAngle = CameraAngle - 0.001 * MsDelta
+    CameraAngle = math.fmod(CameraAngle, math.pi*2)
+  else
+    DrawSpriteScaled(SPlane, 160, 120, planeScale, planeScale, 0x05)
+  end
 else
-  DrawSpriteScaled(SPlane, 160, 120, planeScale, planeScale, 0x05)
+  local drowningTime = Ms - DrowningStartMs
+  if (drowningTime<150) then
+    DrawSpriteScaled(SSplash[1], 160, 160, 1, drowningTime/200, 0x09)
+  elseif (drowningTime<350) then
+    DrawSpriteScaled(SSplash[2], 160, 160, 1, drowningTime/350, 0x09)
+  elseif (drowningTime<2000) then
+    DrawSpriteScaled(SSplash[2], 160, 160, 1, 1, 0x09)
+  elseif (drowningTime<3000) then
+    DrawSpriteScaled(SSplash[1], 160, 160, 1, (3000-drowningTime)/1000, 0x09)
+  elseif (drowningTime<10000) then
+    CameraHeight = TopFlightHeight
+    if (Speed<100) then
+      Speed = 100
+    end
+    CameraX = CameraX - math.sin(CameraAngle) * Speed * 0.0001 * MsDelta
+    CameraY = CameraY + math.cos(CameraAngle) * Speed * 0.0001 * MsDelta
+    if (math.fmod(Ms//200, 2) == 0) then
+      DrawSpriteScaled(SPlane, 160, 120, planeScale, planeScale, 0x05)
+    end
+  else
+    AddEarnings(-10)
+    DrowningStartMs = 0
+  end
 end
 FillRect(310, 182 - Speed*0.5, 10, Speed*0.5, 0xf800)
 
 DrawString(CameraHeight, 30, 30)
+DrawString("Drown" .. DrowningStartMs .. "/" .. (Ms-DrowningStartMs), 30, 40)
 
 SetTextSize(2)
 DisplayEarnings(180, 80)
