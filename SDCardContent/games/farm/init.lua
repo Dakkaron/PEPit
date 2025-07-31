19c4837031a9405b7979b717d89ae534dc144078
Dakkaron <dak1st@gmx.at>
2025-07-31 21:34:21 +0200

    Farm game initial commit

---
+++ SDCardContent/games/farm/init.lua
@@ -0,0 +1,14 @@
+SFarm = {
+    LoadSprite("gfx/farm1.bmp"),
+    LoadSprite("gfx/farm1_c.bmp"),
+    LoadSprite("gfx/farm2.bmp"),
+    LoadSprite("gfx/farm2_c.bmp"),
+    LoadSprite("gfx/farm3.bmp"),
+    LoadSprite("gfx/farm3_c.bmp")
+}
+SFarmer = {
+    LoadAnimSprite("gfx/farmer_f_berry.bmp", 33, 36, 0, 0xf81f),
+    LoadAnimSprite("gfx/farmer_m_berry.bmp", 33, 36, 0, 0xf81f),
+    LoadAnimSprite("gfx/farmer_m_fish.bmp", 33, 31, 0, 0xf81f),
+    LoadAnimSprite("gfx/farmer_m_fish.bmp", 33, 31, 0, 0xf81f),
+}
