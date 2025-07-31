dea9afc1a9fc2f5ed9888b08dcd7a411d16dce35
Dakkaron <dak1st@gmx.at>
2025-07-31 21:29:33 +0200

    Reverted farm game changes to allow merging non-farm stuff

--- SDCardContent/games/farm/longBlow.lua
+++
@@ -1,8 +0,0 @@
-DisableCaching()
-DrawSprite(SFarm[3], 0, 0)
-DrawAnimSprite(SFarmer[1 + (Ms//5000) % 2], 275, 135, (Ms//1000) % 2)
-DrawAnimSpriteScaled(SFarmer[3 + (Ms//5000) % 2], 170, 145, -1.25, 1.25, (Ms//1000) % 2)
-DrawFastVLine(170, 175, 20, 0x0000)
-
-DrawAnimSpriteScaled(SFarmer[1 + (Ms//5000) % 2], 107, 97, -0.7, 0.7, (Ms//1000) % 2)
-DrawAnimSpriteScaled(SFarmer[1 + (Ms//5000) % 2], 120, 115, -0.75, 0.75, (Ms//1000) % 2)
