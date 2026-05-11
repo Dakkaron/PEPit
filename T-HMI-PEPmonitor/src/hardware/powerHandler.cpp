38f1e58a0f3470b39dc0450f59141d66628576d6
Dakkaron <dak1st@gmx.at>
2026-05-11 21:04:25 +0200

    Improvements to deep sleep

--- T-HMI-PEPmonitor/src/hardware/powerHandler.cpp
+++ T-HMI-PEPmonitor/src/hardware/powerHandler.cpp
@@ -23,12 +23,37 @@ bool isSkipSplashScreen() {
 void deepSleepReset() {
     Serial.println("deepSleepReset()");
     skipSplashScreen = true;
-    esp_sleep_enable_timer_wakeup(100);
+    tft.writecommand(0x10);
+    powerDownPressureSensor(true);
+    digitalWrite(M5STACK_JOYSTICK2_POWER_PIN, LOW);
+    SD_MMC.end();
+    buttonPwr.attachClick(nullptr);
+    buttonPwr.attachLongPressStop(nullptr);
+    buttonUsr.attachClick(nullptr);
+    buttonUsr.attachLongPressStop(nullptr);
+
+    delay(50);
+
+    rtc_gpio_init(GPIO_NUM_21);
+    rtc_gpio_set_direction(GPIO_NUM_21, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pullup_dis(GPIO_NUM_21);
+    rtc_gpio_pulldown_dis(GPIO_NUM_21);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_21, 1); // Set power button as wakeup source
+    esp_sleep_enable_timer_wakeup(1000); // Set wakeup timer as wakeup source
     esp_deep_sleep_start();
 }
 
+static bool poweringOff = false;
 void power_off() {
+    if (poweringOff) {
+        return;
+    }
+    poweringOff = true;
     Serial.println("power_off");
+    buttonPwr.attachClick(nullptr);
+    buttonPwr.attachLongPressStop(nullptr);
+    buttonUsr.attachClick(nullptr);
+    buttonUsr.attachLongPressStop(nullptr);
     
     uint32_t lastMs = millis();
     uint32_t ms = millis();
@@ -51,14 +76,17 @@ void power_off() {
     while (gpio_get_level(GPIO_NUM_21)) {
         // Wait for power button to be released
     }
+    delay(50);
 
     Serial.println("Deep sleep starting");
     tft.writecommand(0x10);
     powerDownPressureSensor(true);
     digitalWrite(M5STACK_JOYSTICK2_POWER_PIN, LOW);
     SD_MMC.end();
-    esp_sleep_enable_ext0_wakeup(GPIO_NUM_21, 1); // Set power button as wakeup source
+    rtc_gpio_init(GPIO_NUM_21);
+    rtc_gpio_set_direction(GPIO_NUM_21, RTC_GPIO_MODE_INPUT_ONLY);
     rtc_gpio_pullup_dis(GPIO_NUM_21);
     rtc_gpio_pulldown_dis(GPIO_NUM_21);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_21, 1); // Set power button as wakeup source
     esp_deep_sleep_start();
 }
