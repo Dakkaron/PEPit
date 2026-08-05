caf92447d262181e2007bc41bf3b9521cbd5ee53
Dakkaron <dak1st@gmx.at>
2026-08-05 07:51:46 +0200

    Screen brightness pwm control

--- T-HMI-PEPmonitor/src/main.cpp
+++ T-HMI-PEPmonitor/src/main.cpp
@@ -10,6 +10,9 @@
 #include <SD.h>
 #include <OneButton.h>
 #include "esp_log.h"
+#include "esp_err.h"
+#include "driver/ledc.h"
+#include "esp_pm.h"
 
 #include "hardware/gfxHandler.hpp"
 #include "hardware/pressuresensor.h"
@@ -25,32 +28,59 @@
 #include "systemStateHandler.h"
 #include "hardware/joystickHandler.h"
 
-void setBrightness(uint8_t value) {
-  static uint8_t _brightness = 0;
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          BK_LIGHT_PIN // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#if CONFIG_PM_ENABLE
+#define LEDC_CLK_SRC            LEDC_USE_RC_FAST_CLK // choose a clock source that can maintain during light sleep
+#define LEDC_FREQUENCY          (400) // Frequency in Hertz. Set frequency at 400 Hz
+#else
+#define LEDC_CLK_SRC            LEDC_AUTO_CLK
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+#endif
 
-  if (_brightness == value) {
-    return;
-  }
-  value = _min(16, value);
-  if (value == 0) {
-    digitalWrite(BK_LIGHT_PIN, 0);
-    delay(3);
-    _brightness = 0;
-    return;
-  }
-  if (_brightness == 0) {
+static void example_ledc_init(void) {
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_CLK_SRC,
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0,
+#if CONFIG_PM_ENABLE
+        .sleep_mode     = LEDC_SLEEP_MODE_KEEP_ALIVE,
+#endif
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+/*
+ * value: 0-8191
+ */
+void setBrightness(uint32_t value) {
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, value));
+  // Update duty to apply the new value
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+  //analogWrite(BK_LIGHT_PIN, value);
+  /*if (value == 255) {
     digitalWrite(BK_LIGHT_PIN, 1);
-    _brightness = SCREEN_BRIGHTNESS_STEPS;
-    delayMicroseconds(30);
-  }
-  int from = SCREEN_BRIGHTNESS_STEPS - _brightness;
-  int to = SCREEN_BRIGHTNESS_STEPS - value;
-  int num = (SCREEN_BRIGHTNESS_STEPS + to - from) % SCREEN_BRIGHTNESS_STEPS;
-  for (int i = 0; i < num; i++) {
-    digitalWrite(BK_LIGHT_PIN, 0);
-    digitalWrite(BK_LIGHT_PIN, 1);
-  }
-  _brightness = value;
+  } else {
+    analogWrite(BK_LIGHT_PIN, _max(value, 10));
+  }*/
 }
 
 void setup() {
@@ -59,6 +89,8 @@ void setup() {
   digitalWrite(PWR_ON_PIN, HIGH);
   pinMode(BK_LIGHT_PIN, OUTPUT);
   digitalWrite(BK_LIGHT_PIN, LOW);
+  example_ledc_init();
+  setBrightness(0);
 
   pinMode(M5STACK_JOYSTICK2_POWER_PIN, OUTPUT); // Activate joystick power
   digitalWrite(M5STACK_JOYSTICK2_POWER_PIN, HIGH);
@@ -89,7 +121,6 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
 
   initGfxHandler();
-  setBrightness(16);
   tft.setTextSize(2);
   if (!isSkipSplashScreen()) {
     tft.drawString("Nicht", 10, 10);
